c9dd75ad7c2ac670a4dd9205b53f59ff9e3165f0
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-02-01 21:17:55 +0200

    create task 10

--- task-10.js
+++ task-10.js
@@ -5,9 +5,12 @@ console.table(users);
 // // должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 const getSortedUniqueSkills = users => {
-  let checkSkills = users.reduce((acc,  {skills} ) => [...acc, ...skills], []);
-  checkSkills = checkSkills.sort();
-  return [...new Set(checkSkills)];
+  return users
+  .reduce((acc, item) => {
+    acc.push(...item.skills);
+    return acc.sort()
+    
+  }, []);
 };
 console.table(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
